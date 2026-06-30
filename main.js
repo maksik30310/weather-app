bbc6c5485dc7edeb44ab922d5795ae1530c826eb
max <harlamovmaksim49@gmail.com>
2026-06-30 16:19:13 +0300

    aded mini logic for input

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const weatherCard = document.getElementById('weatherCard'):
+// 1. Находим карточку и инпут по их правильным ID
+const weatherCard = document.getElementById('weatherCard');
+const cityInput = document.getElementById('cityInput');
 
+// 2. Показываем карточку погоды (удаляем класс hidden)
+weatherCard.classList.remove('hidden');
 
-weatherCard.classList.remove('hiddeen');
+// 3. Вешаем слушатель события на инпут
+cityInput.addEventListener('keydown', (event) => {
+
+    if (event.key === 'Enter') {
+        alert('Кликнули Enter! Скоро здесь будет запрос к OpenWeatherMap.');
+    }
+});
